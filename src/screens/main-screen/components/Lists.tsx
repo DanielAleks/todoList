b11ab851bf1c059a2d4079b0835d7adcc7897f5b
danielaleksandrov <crunchyhotline@gmail.com>
2020-12-16 11:04:04 -0800

    connecting lists to todos...

--- src/screens/main-screen/components/Lists.tsx
+++ src/screens/main-screen/components/Lists.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableWithoutFeedbackComponent, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { gloStyles } from '../../../../App'
 import { rootStoreT } from '../../../../store'
 import { ADD_LIST, REMOVE_LIST } from '../../../reducers/types'
-import { TextInput, TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler'
+import { TextInput, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
-function Lists({editMode}) {
+function Lists({ editMode }) {
   const lists = useSelector((state: rootStoreT) => state.lists)
   const [isDropped, setIsDropped] = useState<any>([])
   const [value, setValue] = useState('')
@@ -23,34 +23,27 @@ function Lists({editMode}) {
 
   }
 
-const dropList = (item) => {
-  editMode ? setIsDropped(item.id) : null
-}
-
 
   return (
     <View>
       {lists.map((item) =>
-        <View>
-          <View style={{ display: 'flex', backgroundColor: 'pink', flexDirection: 'row', borderWidth: .25 }}>
-            <TouchableOpacity style={styles.group} onPress={dropList}>
-              <View>
-                <Text style={styles.text} key={item.id}>-{item.value}</Text>
-              </View>
-            </TouchableOpacity>
+        <View key={item.id}>
+          <TouchableWithoutFeedback key={item.id} style={{...styles.group, height: 30, borderWidth: .25}} onPress={() => {
+            isDropped === '' ? setIsDropped(item.id) : setIsDropped('')
+          }}>
+              <Text style={styles.text} key={item.id}>-{item.value}</Text>
 
-          <TouchableOpacity style={gloStyles.button} onPress={() => setIsDropped('')}>
-            <Text style={gloStyles.whiteText}>Remove items under</Text>
-          </TouchableOpacity>
 
-            <TouchableOpacity style={{ display: editMode ? 'flex' : 'none', width: 110, backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center' }} onPress={removeList}>
-              <Text>X</Text>
-            </TouchableOpacity>
-
-          </View>
+              <TouchableOpacity style={{ display: editMode ? 'flex' : 'none', width: 110, backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center' }} onPress={removeList}>
+                <Text>X</Text>
+              </TouchableOpacity>
+          </TouchableWithoutFeedback>
 
           <View>
-            <Text style={{ ...gloStyles.blackText, height: 100, display: isDropped === item.id ? 'flex' : 'none' }}>INFORMATION</Text>
+            <View>
+              <Text style={{ ...gloStyles.blackText, height: '100%', display: isDropped === item.id ? 'flex' : 'none' }}>INFORMATION</Text>
+            </View>
+
           </View>
         </View>
       )}
@@ -64,7 +57,7 @@ const styles = StyleSheet.create({
     height: 50,
     justifyContent: 'center',
     width: '100%',
-    flexDirection: 'row',
+    // flexDirection: 'row',
   },
   text: {
     color: '#e9e9e9',
